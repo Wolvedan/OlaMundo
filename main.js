168a27bd0889b969b5425b72f3b8e3d353b9e323
Daniel Ataides <108036145+Wolvedan@users.noreply.github.com>
2024-05-10 14:19:26 -0300

    .

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-let nomeUsuario = prompt("Qual é o seu nome? (Pressione Cancelar se preferir não inserir um nome)");
+let nomeUsuario = "";
+let elemento = document.querySelector("#nome-usuário");
 
-if (nomeUsuario === null || nomeUsuario.trim() === "") {
-    alert("Você escolheu não inserir um nome. Seja muito bem-vindo.");
-} else {
-    let elemento = document.querySelector("#nome-usuário");
-    elemento.textContent = nomeUsuario;
+while (nomeUsuario.trim() === "") {
+    nomeUsuario = prompt("Qual é o seu nome?");
 }
 
+if (nomeUsuario === null) {
+    elemento.textContent = 'Seja muito bem-vindo.';
+} else {
+    elemento.textContent = nomeUsuario;
+}
 
 
 
