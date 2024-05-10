dfa81aa22bd2f88c3ef4234d5df02a5d955be90b
Daniel Ataides <108036145+Wolvedan@users.noreply.github.com>
2024-05-10 14:03:13 -0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-let elemento = document.querySelector("nome-usuário");
+// Aguarda o carregamento completo do documento HTML
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtém o elemento span com o ID "nome-usuário"
+    var nomeUsuarioElemento = document.getElementById("nome-usuário");
+    
+    // Define o nome do usuário
+    var nomeUsuario = "João";
+    
+    // Atualiza o conteúdo do elemento com o nome do usuário
+    nomeUsuarioElemento.textContent = nomeUsuario;
+});
 
-elemento.textContent = nomeUsuário;
 
 
 
