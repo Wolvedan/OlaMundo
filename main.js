2c71bfa901d2e94e454840cff8917371c3869b61
Daniel Ataides <108036145+Wolvedan@users.noreply.github.com>
2024-05-10 14:11:04 -0300

    .

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
-// Aguarda o carregamento completo do documento HTML
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtém o elemento span com o ID "nome-usuário"
-    var nomeUsuarioElemento = document.getElementById("nome-usuário");
-    
-    // Pede ao usuário que insira seu nome
-    var nomeUsuario = prompt("Por favor, insira seu nome:");
-    
-    // Verifica se o usuário inseriu um nome
-    if (nomeUsuario !== null && nomeUsuario !== "") {
-        // Atualiza o conteúdo do elemento com o nome do usuário
-        nomeUsuarioElemento.textContent = nomeUsuario;
-    } else {
-        // Se o usuário cancelar ou não inserir um nome, exibe uma mensagem padrão
-        nomeUsuarioElemento.textContent = "Fulano";
-    }
-});
+let nomeUsuario = "";
+let elemento = document.querySelector("#nome-usuário");
+
+while (nomeUsuario === "") {
+    nomeUsuario = prompt("Qual é o seu nome?");
+}
+
+if (nomeUsuario === null) {
+    elemento.textContent = 'Seja muito bem-vindo.';
+} else {
+    elemento.textContent = nomeUsuario;
+}
 
 
 
